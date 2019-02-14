a0a7422bbc1dd4983ac878193e3ac5d5add96b65
Charly <charly.martin.1207@gmail.com>
2019-02-14 15:32:32 +0700

    toto

--- javascript/app.js
+++ javascript/app.js
@@ -10,6 +10,10 @@ window.setInterval(runCounter, 100);
 
 // To-Do
 // Change style based on size attributes
+// https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements#Using_the_lifecycle_callbacks
+
 // Ask birthday in a prompt window and store locally
+
 // Create 1 quote & principle / day, no more. Even if page refreshes.
+
 // Have a read-only API key from airtable
